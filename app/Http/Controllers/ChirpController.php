0055169521d475f905c298cb6d646e3f9a017778
tiago <xaloaxolot@gmail.com>
2023-10-25 20:17:20 -0300

    first commit, chirp done

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+use Illuminate\Http\Response;
+
+class ChirpController extends Controller
+{
+    public function index(): View
+    {
+        return view('chirps', []);
+    }
+}
